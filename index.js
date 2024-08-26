4cea9913e4a9efaa895860c3f506b1708b1b14b4
MohamedJainulHaneef <jainulhaneef03@gmail.com>
2024-08-26 17:58:30 +0530

    final

--- index.js
+++ index.js
@@ -1,29 +1,72 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const sequelize_conn = require('./models/dbconnection');
 const staffmaster = require('./models/staffmaster');
 const studentmaster = require('./models/studentmaster');
 
-async function dbconncheck() 
-{
+// async function dbconncheck() 
+// {
+//     try {
+//         // Authenticate the connection
+//         await sequelize_conn.authenticate();
+//         console.log('Database Synced');
+
+//         // Synchronize the staffmaster model
+//         await staffmaster.sync();
+
+//         // Synchronize the studentmaster model
+//         await studentmaster.sync();
+//     } 
+
+//     catch (error) {
+//         console.log('Error Occurred:', error.message);
+//     } 
+
+//     finally {
+//         // Close the database connection
+//         await sequelize_conn.close();
+//     }
+// }
+
+// dbconncheck();
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post('/login', async (req, res) => {
+    const { staff_id, staff_password } = req.body;
+
     try {
-        // Authenticate the connection
-        await sequelize_conn.authenticate();
-        console.log('Database Synced');
+        const user = await staffmaster.findOne({
+            where: {
+                staff_id: staff_id,
+                staff_password: staff_password
+            }
+        });
 
-        // Synchronize the staffmaster model
-        await staffmaster.sync();
-
-        // Synchronize the studentmaster model
-        await studentmaster.sync();
-    } 
-
-    catch (error) {
-        console.log('Error Occurred:', error.message);
-    } 
-
-    finally {
-        // Close the database connection
-        await sequelize_conn.close();
+        if (user) {
+            return res.status(200).json({ success: true, message: "Login Successful" });
+        } else {
+            return res.status(401).json({ success: false, message: "Invalid Credentials" });
+        }
+    } catch (error) {
+        console.error('Error during Login:', error);
+        return res.status(500).json({ success: false, message: "Internal Server Error" });
     }
-}
+});
 
-dbconncheck();
+
+sequelize_conn.authenticate()
+
+.then(() => {
+    console.log('Database connected');
+    app.listen(5000, () => {
+        console.log('Server running on http://localhost:5000');
+    });
+})
+
+.catch(err => {
+    console.error('Unable to connect to the Database:', err);
+});
--- models/dbconnection.js
+++ models/dbconnection.js
@@ -3,7 +3,8 @@ const { Sequelize } = require('sequelize');
 const sequelize_conn = new Sequelize('obev2', 'root', 'haneef9498', 
 {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    logging: false
 });
 
 module.exports = sequelize_conn;
