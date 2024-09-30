85986661223f574e0a7d86eb49221fd055d7184c
therasak <abdulrasak4567@gmail.com>
2024-09-30 20:49:56 +0530

    8_45_30th

--- models/dbconnection.js
+++ models/dbconnection.js
@@ -1,10 +1,6 @@
 const { Sequelize } = require('sequelize');
 
-<<<<<<< HEAD
 const sequelize_conn = new Sequelize('obev2', 'root', 'Therasak.9025',
-=======
-const sequelize_conn = new Sequelize('obev2', 'root', 'hamdhan2003',
->>>>>>> b5c722226a926dbeb962de851ac922a7d1f537ef
     {
         host: 'localhost',
         dialect: 'mysql',
