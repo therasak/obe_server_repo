2992110ed7466cd114577dc40ddb02b9450ec900
sirajhanifa <sirajhanifa786@gmail.com>
2024-11-20 13:29:50 +0530

    new file added

--- routes/studentmanage.js
+++ routes/studentmanage.js
@@ -14,4 +14,33 @@ route.get('/studetails', async (req, res) =>
 
 // ------------------------------------------------------------------------------------------------------- //
 
+// Add new Student 
+
+route.post('/addstudent', async (req, res) => {
+    try {
+      const { stu_name, reg_no, category, section } = req.body;
+  
+      // Create a new student record
+      const newStudent = await studenmaster.create({
+        stu_name,
+        reg_no,
+        category,
+        section,
+      });
+  
+      res.status(200).json({ message: 'Student added successfully!', student: newStudent });
+    } catch (error) {
+      console.error('Error adding student:', error);
+      res.status(500).json({ error: 'Failed to add student' });
+    }
+  });
+
+  // ------------------------------------------------------------------------------------------------------- //
+
+// Delete Student
+
+
+
+  
+
 module.exports = route;
